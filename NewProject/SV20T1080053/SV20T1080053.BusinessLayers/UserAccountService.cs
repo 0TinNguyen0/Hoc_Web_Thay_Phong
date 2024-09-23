50fa10ce5a3c10416e9a8e7764a3ac5875894069
Trung Nguyễn <trunga92loctri@gmail.com>
2024-09-24 00:08:37 +0700

    up

---
+++ NewProject/SV20T1080053/SV20T1080053.BusinessLayers/UserAccountService.cs
@@ -0,0 +1,44 @@
+﻿using SV20T1080053.DataLayers;
+using SV20T1080053.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SV20T1080053.BusinessLayers
+{
+    public static class UserAccountService
+    {
+        private static readonly IUserAccountDAL employeeUserAccountDB;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static UserAccountService()
+        {
+            string connectionString = "server=LAPTOP-9KV8VJ1J;user id=sa;password=123;database=LiteCommerceDB;TrustServerCertificate=true";
+            employeeUserAccountDB = new DataLayers.SQLServer.EmployeeUserAccountDAL(connectionString);
+        }
+
+        public static object Authorize(string userName, string password, TypeOfAccount employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static UserAccount?  userAccount(string userName, string password, TypeOfAccount typeOfAccount)
+        {
+            switch (typeOfAccount)
+            {
+                case TypeOfAccount.Employee:
+                    return employeeUserAccountDB.Authorize(userName, password);
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public enum TypeOfAccount {  Employee, UserAccount,
+        Customer
+    }
+}
